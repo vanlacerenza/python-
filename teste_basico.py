eb6c2788b925c1b098bbcb27d56a7d3d7606dfc8
Vanessa L <van.lacerenza@gmail.com>
2023-05-27 16:23:46 -0300

    Add files via upload

---
+++ teste_basico.py
@@ -0,0 +1,22 @@
+import random
+
+def customerSuccessBalancing(customer_success, customers, customer_success_away):
+
+    for customer_success in customerSucessBalancing:
+        level = customer_success
+
+customers_list = ([(20), (30), (35), (40), (60), (80)])
+clientes_1 = (20,30,35,40)
+clientes_2 = (60,80)
+
+staticmethod
+sorteio1_cs = random.sample(clientes_1,k=2)
+sorteio2_cs = random.sample(clientes_2,k=1)
+
+sorteados = print ("Estes são os clientes que o CS1 nível 50 atenderá " + str(sorteio1_cs), "os demais 2, são do CS 2 que atende nível 50")
+
+sorteados2 = print ("Este é o clientes que o CS3 nível 100 atenderá " + str(sorteio2_cs), "o outro, será do outro CS que atende nível 100")
+
+ 
+
+
